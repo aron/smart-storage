ffdb792c8a083ed844bb10be249de3afafb2a667
Josh Emerson <josh.j.emerson@gmail.com>
2011-07-22 18:00:53 +0100

    Created a lightweight, 1.3kb version just for get and set.
    Often, the only methods required are to get and set values. This wrapper
    supports expiry dates and json values.

--- SmartStorage.js
+++ SmartStorage.js
@@ -25,13 +25,9 @@ THE SOFTWARE.
 * SmartStorage
 * @constructor
 */
-function SmartStorage(dbname, password) {
+function SmartStorage(dbname) {
     if (SmartStorage.browserIsSupported()) {
         this.dbname = dbname;
-        this.password = password || null;
-        this.enc_worker = new Worker("../sjcl.js");
-    } else {
-        throw "SmartStorage error: You should catch this and deal with browsers that don't support localStorage."
     }
 }
 
@@ -48,17 +44,7 @@ SmartStorage.prototype._setItemForDb = function(key, value, time, callback) {
         value = ((new Date()).getTime() + time) + "--cache--" + value;
     }
     
-    // If we're encrypting, then the rest happens in the callback.
-    var me = this;
-    if (SmartStorage.typeOf(this.password) === 'string') {
-        this.enc_worker.onmessage = function(e) {
-            localStorage.setItem(me.dbname + '_' + key, e.data);
-            callback(e.data);
-        }
-        this.enc_worker.postMessage({"set": true, "password": this.password, "value": value })
-    }
-    
-    // Otherwise just set and return values synchronously.
+    // Set and return values synchronously.
     return localStorage.setItem(this.dbname + '_' + key, value);
 }
 
@@ -73,17 +59,7 @@ SmartStorage.prototype._getItemForDb = function(key, callback) {
     var value = localStorage.getItem(prefixed_key);
     if (value) {
         
-        // If we're decrypting, then the rest happens in the callback.
-        if (SmartStorage.typeOf(this.password) === 'string') {
-            this.enc_worker.onmessage = function(e) {
-                value = SmartStorage.getCachableValue(e.data);
-                callback( JSON.parse(value) );
-            }
-            this.enc_worker.postMessage({"password": this.password, "value": value })
-            return;
-        }
-        
-        // Otherwise just return values synchronously.
+        // Return values synchronously.
         value = SmartStorage.getCachableValue(value);
     }
     return JSON.parse(value);
@@ -126,237 +102,6 @@ SmartStorage.prototype.get = function(key, callback) {
     return this._getItemForDb(key, callback);
 }
 
-/**
-* Removes an entry from the browser store.
-* @param {String} key The key to remove.
-*/
-SmartStorage.prototype.remove = function(key) {
-    if (arguments.length < 1) {
-        throw "SmartStorage error: remove() requires 1 argument."
-    }
-    return this._removeItemForDb(key);
-}
-
-/**
-* If key exists and is a string, this appends the value to the end of that string.
-* If key does not exist it creates it, similar to set().
-* @param {String} key The key to store the value against.
-* @param value The value to store
-* @returns The new length of the string.
-*/
-SmartStorage.prototype.append = function(key, value) {
-    if (arguments.length < 2) {
-        throw "SmartStorage error: set() requires 2 arguments."
-    }
-    var existing_value = this._getItemForDb(key);
-    if (existing_value !== null && SmartStorage.typeOf(existing_value) !== 'string') {
-        throw "SmartStorage error: Can only append() to a string."
-    }
-    if (existing_value) {
-        value = existing_value + value;
-    }
-    this._setItemForDb(key, value);
-    return value.length;
-}
-
-/**
-* Increments the stored value by the passed in value. If it does not
-* exist or is of the wrong type the value is set to 0 before incrementing.
-* @param {String} key The key to store the value against.
-* @param {int} increment The number to increment by.
-* @returns The value of the key after the increment.
-*/
-SmartStorage.prototype.incr = function(key, increment) {
-    if (!increment) { // TODO: Deal better with unexpected increment values.
-        increment = 1;
-    }
-    var old_value = this._getItemForDb(key);
-    if (SmartStorage.typeOf(old_value) !== 'number') {
-        old_value = 0;
-    }
-    var new_value = old_value + increment;
-    this._setItemForDb(key, new_value);
-    return new_value;
-}
-
-/**
-* Decrements the stored value by 1. If it does not exist or is of the
-* wrong type the value is set to 0 before decrementing.
-* @param {String} key The key to decrement.
-* @returns The value of the key after the decrement.
-*/
-SmartStorage.prototype.decr = function(key, increment) {
-    if (!increment) { // TODO: Deal better with unexpected increment values.
-        increment = 1;
-    }
-    increment = -increment;
-    return this.incr(key, increment);
-}
-
-/**
-* Renames key to newkey. Throws an error if key and newkey are the
-* same, or if key does not exist. Overwrites existing newkey.
-* @param {String} key The key to rename.
-* @param {String} newkey The new key name.
-*/
-SmartStorage.prototype.rename = function(key, newkey) {
-    if (key === newkey) {
-        throw "SmartStorage error: Cannot rename key to itself."
-    }
-    var value = this._getItemForDb(key);
-    if (value === null) {
-        throw "SmartStorage error: Cannot rename non-existant key."
-    }
-    this._setItemForDb(newkey, this._getItemForDb(key));
-    this._removeItemForDb(key);
-}
-
-/**
-* Get the length of a string in the store.
-* @param {String} key The key.
-* @returns Length of string, or 0 if key not set.
-*/
-SmartStorage.prototype.strlength = function(key) {
-    var value = this._getItemForDb(key);
-    if (SmartStorage.typeOf(value) !== 'string') {
-        if (value === null) {
-            value = "";
-        } else {
-            throw "SmartStorage error: Value must be a string to test its length."
-        }
-        
-    }
-    return value.length;
-}
-
-/**
-* Set key to value if no key is set there.
-* @param {String} key The key.
-* @param value The value to set.
-* @returns true if the key was set, false if not.
-*/
-SmartStorage.prototype.setnx = function(key, value) {
-    if (this._getItemForDb(key) === null) {
-        this.set(key, value);
-        return true;
-    }
-    return false;
-}
-
-/**
-* Set key to value if no key is set there.
-* @param {String} key The key.
-* @param value The value to set.
-* @returns true if the key was set, false if not.
-*/
-SmartStorage.prototype.getset = function(key, value) {
-    var old_value = this._getItemForDb(key);
-    this.set(key, value);
-    return old_value;
-}
-
-/**
-* Set expiry time on a key.
-* @param {String} key The key.
-* @param time The time to live in milliseconds.
-* @returns true if the key existed and expiry has been applied, false if not.
-*/
-SmartStorage.prototype.expire = function(key, time) {
-    var value = this._getItemForDb(key);
-    if (value !== null) {
-        this.set(key, value, time);
-        return true;
-    }
-    return false;
-}
-
-/**
-* Remove expiry time on a key.
-* @param {String} key The key.
-* @returns true if the key existed and expiry has been cancelled, false if not.
-*/
-SmartStorage.prototype.persist = function(key) {
-    var value = this._getItemForDb(key);
-    if (value !== null) {
-        this.set(key, value);
-        return true;
-    }
-    return false;
-}
-
-/**
-* Push a value on to an array. If no existing array then create it.
-* @param {String} key The key of the array.
-* @param value The value to push on to the array
-* @returns the length of the array after push.
-*/
-SmartStorage.prototype.push = function(key, value) {
-    var val = this._getItemForDb(key);
-    if (val === null) {
-        val = [];
-    } else 
-    if (SmartStorage.typeOf(val) !== 'array') {
-        throw "SmartStorage error: Value must be an array to push a value on it."
-    }
-    val.push(value);
-    this.set(key, val);
-    return val.length
-}
-
-/**
-* Pop a value from the end of an array.
-* @param {String} key The key of the array.
-* @returns the length of the array after push.
-*/
-SmartStorage.prototype.pop = function(key) {
-    var val = this._getItemForDb(key);
-    if (val === null) {
-        throw "SmartStorage error: Cannot pop from non-existant key."
-    }
-    if (SmartStorage.typeOf(val) !== 'array') {
-        throw "SmartStorage error: Value must be an array to pop a value from it."
-    }
-    var popped_val = val.pop();
-    this.set(key, val);
-    return popped_val;
-}
-
-/**
-* Add a value on to the beginning of an array. If no existing array then create it.
-* @param {String} key The key of the array.
-* @param value The value to prepend to the array
-* @returns the length of the array after unshift.
-*/
-SmartStorage.prototype.unshift = function(key, value) {
-    var val = this._getItemForDb(key);
-    if (val === null) {
-        val = [];
-    } else 
-    if (SmartStorage.typeOf(val) !== 'array') {
-        throw "SmartStorage error: Value must be an array to prepend a value to it."
-    }
-    val.unshift(value);
-    this.set(key, val);
-    return val.length
-}
-
-/**
-* Remove a value from the beginning of an array.
-* @param {String} key The key of the array.
-* @returns the length of the array after shift.
-*/
-SmartStorage.prototype.shift = function(key) {
-    var val = this._getItemForDb(key);
-    if (val === null) {
-        throw "SmartStorage error: Cannot pop from non-existant key."
-    }
-    if (SmartStorage.typeOf(val) !== 'array') {
-        throw "SmartStorage error: Value must be an array to pop a value from it."
-    }
-    var shift_val = val.shift();
-    this.set(key, val);
-    return shift_val;
-}
 
 SmartStorage.getCachableValue = function(value) {
     if (value.indexOf('--cache--') > -1) {
